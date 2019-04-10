1b2d7833330dfd6f898eb433d911ef7eac2eb19a
Marvin Stelter <41212885+MarvinStelter@users.noreply.github.com>
2019-04-10 23:05:54 +0200

    simple template (php isn't required)

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog - Template by Marvin Stelter</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+      <!-- Blog starts here-->
+	<div class="card">
+            <header>
+                <img src="img/test.png" width="192" height="192"/>
+                <h1>Title Header</h1>
+            </header>
+                <p>Blog content goes here.</p>
+            <hr>
+        </div>
+		
+	    
+		<br>
+	   <!-- Blog endes here-->
+        <footer>
+           <div class="social">
+              <a href="#" id="text_colorfull">Footer Link</a>
+              <a href="#" id="text_colorfull">Footer Link</a>
+              <a href="#" id="text_colorfull">Footer Link</a>
+           </div>
+        </footer>
+    </body>
+</html>
